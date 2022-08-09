a95b9975ec2ba28f83682c1f204a35b46bca8eed
carlosfabioa <carlosfabioa@hotmail.com>
2022-08-09 16:24:26 -0300

    030 Dia

---
+++ 30 dia/Pessoa.java
@@ -0,0 +1,57 @@
+package desafio100Dias.dia30;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Pessoa {
+	private JTextField tfId, tfNome, tfEmail;
+	private JLabel lbId, lbNome, lbEmail;
+	private JButton btInserir, btExcluir;
+	
+	public Pessoa(){
+		JFrame f = new JFrame("Cadastro");
+		
+		lbId = new JLabel("ID: ");
+		lbId.setBounds(30, 50, 150,20);
+		
+		tfId = new JTextField();
+		tfId.setBounds(100,50, 80,20);
+		
+		lbNome = new JLabel("Nome: ");
+		lbNome.setBounds(30,80, 150,20);
+	
+		tfNome = new JTextField();
+		tfNome.setBounds(100,80, 200,20);
+		
+		lbEmail = new JLabel("E-mail: ");
+		lbEmail.setBounds(30, 110, 150, 20);
+		
+		tfEmail = new JTextField();
+		tfEmail.setBounds(100,110, 200,20);
+		
+		btInserir = new JButton("Inserir");
+		btInserir.setBounds(100, 160, 80, 30);
+		
+		btExcluir = new JButton("Excluir");
+		btExcluir.setBounds(200, 160, 80, 30);
+		
+		f.add(lbId);
+		f.add(tfId);
+		f.add(tfNome);
+		f.add(lbNome);
+		f.add(lbEmail);
+		f.add(tfEmail);
+		f.add(btInserir);
+		f.add(btExcluir);
+		
+		f.setSize(400,300);
+		f.setLayout(null);
+		f.setVisible(true);
+		
+		
+	}
+	
+
+}
---
+++ 30 dia/TestePessoa.java
@@ -0,0 +1,10 @@
+package desafio100Dias.dia30;
+
+public class TestePessoa {
+
+	public static void main(String[] args) {
+		Pessoa pessoa = new Pessoa();
+
+	}
+
+}
